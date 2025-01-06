e683898df3a9b4daa3b818574ed8a06de8351c49
AravindCodesAllDay <aravindsiva1509@gmail.com>
2025-01-06 23:14:56 +0530

    sponsors

--- app/partners/sponsers.tsx
+++ app/partners/sponsers.tsx
@@ -1,37 +1,43 @@
 import Image from "next/image";
-
 import { sponsors } from "@/sponsors";
 
 export default function Sponsors() {
   return (
-    <div className="relative min-h-96 p-3 flex flex-col items-center justify-center w-full text-white bg-sponsor bg-cover bg-center bg-no-repeat">
-      <div className="text-center mb-8">
-        <p className="text-sm uppercase tracking-widest">Who Helps Us</p>
-        <h2 className="text-3xl font-bold">Our Sponsors</h2>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 mx-auto mt-2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941"
-          />
-        </svg>
-      </div>
+    <div className="relative min-h-96 p-6 flex flex-col items-center justify-center w-full bg-sponsor bg-cover bg-center bg-no-repeat">
+      <h2 className="text-2xl sm:text-3xl font-bold text-center my-8">
+        Our <span className="text-primary">Prestigious Sponsors</span>
+      </h2>
 
-      <div className="flex flex-col sm:flex-row gap-12 items-center justify-around w-full px-4">
-        {sponsors.map((sponsor, index) => (
-          <Image
-            key={index}
-            src={sponsor.photo}
-            alt={sponsor.name}
-            className="w-auto h-10 md:h-12 object-contain hover:scale-105 transition-all duration-200 ease-in-out"
-          />
+      <div className="flex flex-col gap-16 w-full max-w-5xl px-4">
+        {sponsors.map((involve, involveIndex) => (
+          <div
+            key={involveIndex}
+            className="w-full flex flex-col gap-6 items-center sm:items-start"
+          >
+            <h3 className="text-xl text-primary font-semibold text-center sm:text-left">
+              {involve.title}
+            </h3>
+
+            <div className="flex flex-wrap items-center gap-4 justify-center w-full">
+              {involve.sponsors.map((sponsor, sponsorIndex) => (
+                <div
+                  className="size-20 sm:size-32 md:size-40 relative"
+                  key={sponsorIndex}
+                >
+                  <Image
+                    src={sponsor.photo}
+                    alt={sponsor.name}
+                    fill
+                    sizes="(max-width: 640px) 80px, 
+                           (max-width: 768px) 100px, 
+                           (max-width: 1024px) 120px, 
+                           150px"
+                    className="object-contain hover:scale-105 transition-all duration-200 ease-in-out"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         ))}
       </div>
     </div>
--- sponsors.ts
+++ sponsors.ts
@@ -1,18 +1,103 @@
-import sponsor1 from "@/public/sponsors/Tech-Mahindra.png";
-import sponsor2 from "@/public/sponsors/Honeywell.png";
-import sponsor3 from "@/public/sponsors/sterlite-technologies.png";
+import sponsor1 from "@/public/sponsors/A2000.png";
+import sponsor2 from "@/public/sponsors/HCL.png";
+import sponsor17 from "@/public/sponsors/hexaware.png";
+import sponsor4 from "@/public/sponsors/l&t.png";
+import sponsor5 from "@/public/sponsors/MeitY.png";
+import sponsor7 from "@/public/sponsors/dailyhunt.png";
+import sponsor8 from "@/public/sponsors/edr.jpg";
+import sponsor9 from "@/public/sponsors/infosys.png";
+import sponsor10 from "@/public/sponsors/polycab.png";
+import sponsor11 from "@/public/sponsors/sairam.png";
+import sponsor13 from "@/public/sponsors/stpi.png";
+import sponsor14 from "@/public/sponsors/tcs.png";
+import sponsor15 from "@/public/sponsors/vit.png";
+import sponsor16 from "@/public/sponsors/wipro.png";
 
 export const sponsors = [
   {
-    name: "Tech Mahindra",
-    photo: sponsor1,
+    title: "Co-Host Partner",
+    sponsors: [
+      {
+        photo: sponsor13,
+        name: "STPI",
+      },
+    ],
   },
   {
-    name: "Honeywell",
-    photo: sponsor2,
+    title: "Theme Partner",
+    sponsors: [
+      {
+        photo: sponsor14,
+        name: "TCS",
+      },
+
+      {
+        photo: sponsor2,
+        name: "HCL tech",
+      },
+
+      {
+        photo: sponsor17,
+        name: "Hexaware",
+      },
+    ],
   },
   {
-    name: "Sterlite Technologies",
-    photo: sponsor3,
+    title: "Session Partner",
+    sponsors: [
+      {
+        photo: sponsor8,
+        name: "edr",
+      },
+
+      {
+        photo: sponsor9,
+        name: "infosys",
+      },
+
+      {
+        photo: sponsor16,
+        name: "wipro",
+      },
+
+      {
+        photo: sponsor1,
+        name: "A2000",
+      },
+
+      {
+        photo: sponsor15,
+        name: "VIT",
+      },
+
+      {
+        photo: sponsor10,
+        name: "polycab",
+      },
+
+      {
+        photo: sponsor5,
+        name: "meitY",
+      },
+
+      {
+        photo: sponsor11,
+        name: "sairam",
+      },
+
+      {
+        photo: sponsor4,
+        name: "L&T",
+      },
+    ],
+  },
+  {
+    title: "Media Partner",
+    sponsors: [
+      {
+        photo: sponsor7,
+        name: "Daily Hunt",
+      },
+    ],
   },
 ];
